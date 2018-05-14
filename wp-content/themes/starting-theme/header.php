1f340a2700e6eaf9aef1bfff496569f5f3430fcc
Web Dev <webdev@Webs-MBP.lan>
2018-05-14 12:26:39 +0100

    added metadata

--- wp-content/themes/starting-theme/header.php
+++ wp-content/themes/starting-theme/header.php
@@ -15,6 +15,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link type="text/plain" rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt" />
+<link type="text/plain" rel="author" href="<?php echo get_template_directory_uri(); ?>/robots.txt" />
+<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.svg">
 <?php wp_head(); ?>
 </head>
 
