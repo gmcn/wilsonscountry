80d7a9fed8a9e8c7bc010e4840f563666e8de07f
Web Dev <webdev@Webs-MBP.lan>
2018-06-26 15:44:24 +0100

    show video if post thumbnail not selected

--- wp-content/themes/starting-theme/inc/page-blog/blog.php
+++ wp-content/themes/starting-theme/inc/page-blog/blog.php
@@ -38,11 +38,43 @@ $the_query = new WP_Query( $args ); ?>
         <?php //the_content() ?>
         <a class="more" href="<?php the_permalink(); ?>">Read More ></a>
       </div>
-      <div class="col-md-6 blogmatch" style="background: url('<?php echo $thumb['0'];?>') #EDEDED center; background-size: cover;">
-        <span>
-          <?php the_date('d.m.y'); ?>
-        </span>
-      </div>
+
+      <?php if( have_rows('gallery') ): ?>
+
+        <?php $i=2; while( have_rows('gallery') ): the_row();
+
+          // vars
+          $image_video = get_sub_field('image_video');
+          $gallery_image = get_sub_field('gallery_image');
+          $video_link = get_sub_field('video_link');
+          $video_placeholder = get_sub_field('video_placeholder');
+          $media_title = get_sub_field('media_title');
+          $featuredimg = get_the_post_thumbnail();
+
+          ?>
+
+          <?php if (!$featuredimg && $i == '2' && $image_video == 'Video') : ?>
+
+            <div class="col-md-6 blogmatch">
+              <iframe width="100%" height="100%" src="<?php echo $video_link ?>?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+            </div>
+
+          <?php else : ?>
+
+
+            <div class="col-md-6 blogmatch" style="background: url('<?php echo $thumb['0'];?>') #EDEDED center; background-size: cover;">
+              <span>
+                <?php the_date('d.m.y'); ?>
+              </span>
+            </div>
+
+
+          <?php endif ?>
+
+        <?php $i++; endwhile; ?>
+
+      <?php endif; ?>
     </div>
 
 
